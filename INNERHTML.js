7620586b44d279159a4b92985863f72502226e53
KenRo-hub <64104357+KenRo-hub@users.noreply.github.com>
2020-07-06 14:58:53 -0500

    Add files via upload

---
+++ INNERHTML.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    const parrafo = document.getElementById('text')
+    // console.log(parrafo.innerText = '<li>Elemento 1</li><li>Elemento 2</li>')
+    parrafo.innerHTML = '<li>Elemento 1</li><li>Elemento 2</li>' 
+}
+
+
